d8e2b44080fb161b073b5e0b050391c698d5e62d
Lut99 <mullertim99@gmail.com>
2022-02-21 13:53:35 +0100

    Added very simple new package to test building

---
+++ examples/helloworld/hello_world.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# HELLO WORLD.sh
+#   by Lut99
+#
+# Created:
+#   21 Feb 2022, 13:46:57
+# Last edited:
+#   21 Feb 2022, 13:52:30
+# Auto updated?
+#   Yes
+#
+# Description:
+#   Simple script that returns 'Hello, world!' in such a way that the Brane
+#   framework can extract it as a string.
+#
+
+# Simply echo the hello world with the specified format
+echo "output: Hello there!"
